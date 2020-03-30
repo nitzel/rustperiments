f0dbf402d23a1ffe281de5f8571ee993a786c4ef
nitzel <jan.s.92@web.de>
2020-03-30 21:35:03 +0100

    [RLTK:CH2.3] User input: player moving with keys

--- roguelikerust/src/main.rs
+++ roguelikerust/src/main.rs
@@ -1,8 +1,7 @@
 use rltk::{Console, GameState, Rltk, RGB, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
-#[macro_use]
-extern crate specs_derive;
+use specs_derive::*;
 
 #[derive(Component)]
 struct Position {
@@ -20,22 +19,65 @@ struct Renderable {
 #[derive(Component)]
 struct LeftMover {}
 
-struct LeftWalker{}
+#[derive(Component, Debug)]
+struct Player {}
 
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+struct State {
+    ecs: World
+}
 
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for(_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79 , max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
     }
 }
 
+struct LeftWalker {}
 
-struct State {
-    ecs: World,
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                        WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty,pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
 }
 
 impl State {
@@ -46,44 +88,29 @@ impl State {
     }
 }
 
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
-
-        self.run_systems();
-
-        ctx.cls();
-        ctx.print(1, 1, "Hello Rost");
-
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-        for(pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
 fn main() {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-        .with_title("Rostig")
+        .with_title("Roguelike Tutorial")
         .build();
-
-    let mut gs = State{
+    let mut gs = State {
         ecs: World::new()
     };
-
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
-    gs.ecs.create_entity()
-          .with(Position { x: 40, y: 25 })
-          .with(Renderable {
-              glyph: rltk::to_cp437('@'),
-              fg: RGB::named(rltk::YELLOW),
-              bg: RGB::named(rltk::BLACK),
-          })
-          .build();
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player{})
+        .build();
 
     for i in 0..10 {
         gs.ecs
