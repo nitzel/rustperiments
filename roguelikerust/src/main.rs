ab4117dfc6a5aeaa3c2cd3824b961d97f33eccf3
nitzel <jan.s.92@web.de>
2020-03-30 21:12:11 +0100

    [RLTK:CH2.1] Rendering entities/components

--- roguelikerust/src/main.rs
+++ roguelikerust/src/main.rs
@@ -1,18 +1,72 @@
-use rltk::{Rltk, GameState, Console};
+use rltk::{Console, GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+#[macro_use]
+extern crate specs_derive;
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: u8,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World,
+}
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        ctx.print(1, 1, "Hello Rost");
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for(pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
 fn main() {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
+        .with_title("Rostig")
         .build();
-    let gs = State{ };
+
+    let w = World::new();
+    let mut gs = State{
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs.create_entity()
+          .with(Position { x: 40, y: 25 })
+          .with(Renderable {
+              glyph: rltk::to_cp437('@'),
+              fg: RGB::named(rltk::YELLOW),
+              bg: RGB::named(rltk::BLACK),
+          })
+          .build();
+
+    for i in 0..10 {
+        gs.ecs
+        .create_entity()
+        .with(Position { x: i * 7, y: 20 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    }
+
     rltk::main_loop(context, gs);
 }
